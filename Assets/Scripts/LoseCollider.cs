81bcaf8324851490996b2f3dcf2758e8a5853db7
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-19 18:58:24 -0500

    Movement By Mouse,Physics Materials ando others

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+    public LevelManager_CS LevelManager;
+    private void OnTriggerEnter2D(Collider2D trigger)
+    {
+        print("Trigger");
+        LevelManager.LoadLevel("Lose");
+    }
+
+    
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    float MousePosInBlocks;
+    public float MaxRange_X = 15.5f;
+    public float MinRange_X = 0.5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 PaddlePos = new Vector3(MousePosInBlocks, this.transform.position.y, 0);
+        MousePosInBlocks = Input.mousePosition.x/Screen.width * 16 ;
+        PaddlePos.x=  Mathf.Clamp(MousePosInBlocks, MinRange_X, MaxRange_X);
+        this.transform.SetPositionAndRotation(PaddlePos,Quaternion.identity);
+    }
+}
