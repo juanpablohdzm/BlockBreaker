f786e60369925b03168db82f787adb95864d7442
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-19 13:53:53 -0500

    Singletons

---
+++ Assets/Scripts/LevelManager_CS.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager_CS : MonoBehaviour {
+
+	public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name,LoadSceneMode.Single);
+    }
+
+    public void QuitRequest()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+
+    //Object responsible for music of the entire game
+    static MusicPlayer instance = null;
+
+    private void Awake()
+    {
+        print("Awake: " + GetInstanceID());
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        print("Start: " + GetInstanceID());
+        if (instance != null)
+            Destroy(gameObject);
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   
+}
