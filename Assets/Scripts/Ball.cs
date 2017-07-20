fcfed02bbcefbb38d6750b8cd0568d130da1880c
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-19 19:31:51 -0500

    Launch Ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
-
+    public Paddle paddle;
+    private Vector3 PaddleToBallVector;
+    private bool HasStarted=false;
 	// Use this for initialization
 	void Start () {
-		
+        PaddleToBallVector = this.transform.position-paddle.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (!HasStarted)
+        {
+            this.transform.position = paddle.transform.position + PaddleToBallVector;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                HasStarted = true;
+                GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+            }
+        }
 	}
 }
